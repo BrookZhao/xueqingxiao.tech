0291e236abaa324a0f071339b0be0cc5531976bc
Shawn Sit <xueqingxiao@gmail.com>
2017-03-18 20:16:45 +0800

    init

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { IndexRoute, Route } from 'react-router';
-import { Home } from 'modules/Home';
-
-export default (
-  <Route path="/" component={Home}>
-    <IndexRoute path="/" component={Home} />
-  </Route>
-);
