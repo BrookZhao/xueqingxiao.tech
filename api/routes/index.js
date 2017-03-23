1ce4722b91320a4c09f71b4e30df3915690ca825
Shawn Sit <xueqingxiao@gmail.com>
2017-03-23 23:04:45 +0800

    add all routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,20 @@
 const Router = require('koa-router');
+const comment = require('./comment');
 const navigation = require('./navigation');
+const series = require('./series');
+const story = require('./story');
+const tag = require('./tag');
+const user = require('./user');
 
 const router = new Router({
   prefix: '/api',
 });
 
+router.use(comment.routes());
 router.use(navigation.routes());
+router.use(series.routes());
+router.use(story.routes());
+router.use(tag.routes());
+router.use(user.routes());
 
 module.exports = router;
