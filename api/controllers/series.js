d8f11d1d8f65d9d91c65215938b837e4edde1eff
Shawn Sit <xueqingxiao@gmail.com>
2017-03-27 16:05:22 +0800

    Refactor series

--- api/controllers/series.js
+++ api/controllers/series.js
@@ -1,46 +1,57 @@
 const Series = require('../models/Series');
+const { getErrorPaylod } = require('../utils');
 
-const add = async (ctx, next) => {
+const add = async(ctx, next) => {
   ctx.checkBody('name').notEmpty();
   ctx.checkBody('description').notEmpty();
   ctx.checkBody('logo').notEmpty();
-  ctx.body = {
-    series: await ctx.mongoose.addOne(Series, ctx.request.body)
-  };
+  if (ctx.errors) {
+    ctx.body = getErrorPaylod(Navigation, ctx.errors);
+    return;
+  }
+  ctx.body = await ctx.mongoose.addOne(Series, ctx.request.body);
   await next();
 };
 
-const remove = async (ctx, next) => {
+const remove = async(ctx, next) => {
   ctx.checkParams('id').notEmpty();
-  ctx.body = {
-    series: await ctx.mongoose.deleteOne(Series, ctx.params)
-  };
+  if (ctx.errors) {
+    ctx.body = getErrorPaylod(Navigation, ctx.errors);
+    return;
+  }
+  ctx.body = await ctx.mongoose.deleteOne(Series, ctx.params);
   await next();
 };
 
-const update = async (ctx, next) => {
+const update = async(ctx, next) => {
   ctx.checkParams('id').notEmpty();
   ctx.checkBody('name').optional().notEmpty();
   ctx.checkBody('description').optional().notEmpty();
   ctx.checkBody('logo').optional().notEmpty();
-  ctx.body = {
-    series: await ctx.mongoose.updateOne(Series, ctx.params, ctx.request.body)
-  };
+  if (ctx.errors) {
+    ctx.body = getErrorPaylod(Navigation, ctx.errors);
+    return;
+  }
+  ctx.body = await ctx.mongoose.updateOne(Series, ctx.params, ctx.request.body);
   await next();
 };
 
-const getOne = async (ctx, next) => {
+const getOne = async(ctx, next) => {
   ctx.checkParams('id').notEmpty();
-  ctx.body = {
-    series: await ctx.mongoose.getOne(Series, ctx.params)
-  };
+  if (ctx.errors) {
+    ctx.body = getErrorPaylod(Navigation, ctx.errors);
+    return;
+  }
+  ctx.body = await ctx.mongoose.getOne(Series, ctx.params);
   await next();
 };
 
-const getAll = async (ctx, next) => {
-  ctx.body = {
-    series: await ctx.mongoose.getAll(Series, {})
-  };
+const getAll = async(ctx, next) => {
+  if (ctx.errors) {
+    ctx.body = getErrorPaylod(Navigation, ctx.errors);
+    return;
+  }
+  ctx.body = await ctx.mongoose.getAll(Series, {});
   await next();
 };
 
