9deffc724b9178fc2e51022ff3103b66f266b465
Shawn Sit <xueqingxiao@gmail.com>
2017-03-27 16:04:32 +0800

    Refactor Navigation

--- api/controllers/navigation.js
+++ api/controllers/navigation.js
@@ -1,19 +1,24 @@
 const Navigation = require('../models/Navigation');
+const { getErrorPaylod } = require('../utils');
 
 const add = async (ctx, next) => {
   ctx.checkBody('name').notEmpty();
   ctx.checkBody('link').notEmpty();
-  ctx.body = {
-    navigation: await ctx.mongoose.addOne(Navigation, ctx.request.body)
-  };
+  if (ctx.errors) {
+    ctx.body = getErrorPaylod(Navigation, ctx.errors);
+    return;
+  }
+  ctx.body = await ctx.mongoose.addOne(Navigation, ctx.request.body);
   await next();
 };
 
 const remove = async (ctx, next) => {
   ctx.checkParams('id').notEmpty();
-  ctx.body = {
-    navigation: await ctx.mongoose.deleteOne(Navigation, ctx.params),
-  };
+  if (ctx.errors) {
+    ctx.body = getErrorPaylod(Navigation, ctx.errors);
+    return;
+  }
+  ctx.body = await ctx.mongoose.deleteOne(Navigation, ctx.params);
   await next();
 };
 
@@ -21,29 +26,31 @@ const update = async (ctx, next) => {
   ctx.checkParams('id').notEmpty();
   ctx.checkBody('name').optional().notEmpty();
   ctx.checkBody('link').optional().notEmpty();
-  ctx.body = {
-    navigation: await ctx.mongoose.updateOne(
-      Navigation,
-      ctx.params,
-      ctx.request.body
-    ),
-  };
+  if (ctx.errors) {
+    ctx.body = getErrorPaylod(Navigation, ctx.errors);
+    return;
+  }
+  ctx.body = await ctx.mongoose.updateOne(Navigation, ctx.params, ctx.request.body);
   await next();
 };
 
 const getOne = async (ctx, next) => {
   ctx.checkParams('id').notEmpty();
-  ctx.body = {
-    navigation: await ctx.mongoose.getOne(Navigation, ctx.params),
-  };
+  if (ctx.errors) {
+    ctx.body = getErrorPaylod(Navigation, ctx.errors);
+    return;
+  }
+  ctx.body = await ctx.mongoose.getOne(Navigation, ctx.params);
   await next();
 };
 
 const getAll = async (ctx, next) => {
   const navigations = await Navigation.find({});
-  ctx.body = {
-    navigations: await ctx.mongoose.getAll(Navigation, ctx.query),
-  };
+  if (ctx.errors) {
+    ctx.body = getErrorPaylod(Navigation, ctx.errors);
+    return;
+  }
+  ctx.body = await ctx.mongoose.getAll(Navigation, ctx.query);
   await next();
 };
 
